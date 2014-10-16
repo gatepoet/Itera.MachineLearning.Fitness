224908dd2294515c5df69e2e07e43c1279b40c05
Kristoffer Rolf Deinoff <kristoffer.deinoff@gmail.com>
2014-10-16 16:23:50 +0200

    Added SportsTracker Data

--- Services/FitnessService.cs
+++ Services/FitnessService.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+using numl.Math.LinearAlgebra;
 
 namespace Itera.MachineLearning.Fitness.Services
 {
@@ -19,15 +20,15 @@ namespace Itera.MachineLearning.Fitness.Services
 
         public IEnumerable<Activity> GetActivities()
         {
-            return GetRunkeeperActivities();
+            return GetRunkeeperActivities().Union(
+                GetSportsTrackerActivities());
         }
 
-        private IEnumerable<Activity> GetRunkeeperActivities()
+        public IEnumerable<Activity> GetRunkeeperActivities()
         {
             var path = Path.Combine(
                 BasePath,
                 @"Runkeeper\cardioActivities.csv");
-                
             
             return File.ReadAllLines(path)
                 .Skip(1)
@@ -75,5 +76,62 @@ namespace Itera.MachineLearning.Fitness.Services
                 : TimeSpan.ParseExact(value, "h\\:m\\:ss", Culture);
         }
 
+        XNamespace stNs = "http://www.polarpersonaltrainer.com";
+
+        public IEnumerable<Activity> GetSportsTrackerActivities()
+        {
+
+            var document = GetPolarXML();
+            return document
+                .Descendants(stNs + "polar-exercise-data")
+                .Descendants(stNs + "calendar-items")
+                .Descendants(stNs + "exercise")
+                .Select(CreateSportsTrackerActivity);
+        }
+
+        private Activity CreateSportsTrackerActivity(XElement xml)
+        {
+            ActivityType type;
+            var typeString = xml.Element(stNs + "sport").Value;
+            if (!Enum.TryParse(typeString, out type))
+            {
+                switch (typeString)
+                {
+                    case "Cross-country":
+                        type = ActivityType.Skiing;
+                        break;
+                    default:
+                        type = ActivityType.Other;
+                        break;
+                }
+            }
+            var result = xml.Element(stNs + "result");
+            var distance = GetDouble(GetValue(result.Element(stNs + "distance")));
+            var duration = TimeSpan.Parse(result.Element(stNs + "duration").Value);
+            
+            return new Activity
+            {
+                Date = DateTime.Parse(xml.Element(stNs + "time").Value),
+                Type = type,
+                Distance = distance/1000.0,
+                Duration = duration,
+                AverageSpeed = distance/duration.TotalHours * 1.6
+            };
+        }
+
+        private string GetValue(XElement element)
+        {
+            return element == null ? "" : element.Value;
+        }
+
+        public static XDocument GetPolarXML()
+        {
+            var path = Path.Combine(
+                BasePath,
+                @"PolarPersonalTrainer\Travelisio_15.10.2014_export.xml");
+
+            var document = XDocument.Load(path);
+            return document;
+        }
     }
 }
